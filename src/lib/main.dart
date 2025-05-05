4bb94d15e70076bff4b453f07cd37f2b96d43ebd
Abner Rodrigues <abnerwcrodrigues@outlook.com>
2025-05-04 22:09:18 -0300

    feat: Adiciona Login Page sem estilização

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:burnquest/services/auth_service.dart';
+import 'package:burnquest/viewmodels/login_viewmodel.dart';
 import 'package:burnquest/views/base_view.dart';
+import 'package:burnquest/views/login_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +15,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(title: 'Burnquest'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => LoginViewModel(AuthService()),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Login Page',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const LoginView(),
+      )
     );
   }
 }
